eb47d0e4b0ae7397bb55602a05cc4d6d1a79fe29
DungAhihi <tuandungn50@gmail.com>
2023-04-26 22:39:58 +0700

    Cấu hình Router/layout
    tải react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+// tk Fragment chỉ để chứa chứ không sinh ra thẻ trong DOM
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
-    return <div className="App">React app</div>;
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <route.component />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+            ;
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+// layout
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/search', component: Search, layout: null },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+export { publicRoutes };
